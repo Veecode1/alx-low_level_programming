2b5bcc1955c097446009186dc4ed29e94c5502c9
Veecode1 <chantelvic@gmail.com>
2023-06-15 08:18:58 -0700

    name-add

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - the entry point of the function
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle\n");
+		return (0);
+}
+
