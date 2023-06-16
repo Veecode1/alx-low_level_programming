ee36180392b2323a2224fdd54889440a4defa566
Veecode1 <chantelvic@gmail.com>
2023-06-16 08:54:54 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,16 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
 	if (n > 0)
-	{
 		printf("%d is positive", n);
-	}
 	else if (n == 0)
-	{
 		printf("%d is zero", n);
 	else
-	{
-		printf("%d is negetive", n);
-	}
+		printf("%d is negetive", n);`
 	printf("\n");
 
 	return (0);
