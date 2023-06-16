d861d2f73fd0537b0805b1cca6ddcf6fd44ba870
Veecode1 <chantelvic@gmail.com>
2023-06-16 13:36:40 -0700

    digit-alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * A program that prints the alphabet in lowercase.
+ * main - The starting point of the function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+		putchar('\n');
+	return(0);
+}
